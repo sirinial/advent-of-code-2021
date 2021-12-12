92e8f8bc3099c4e65292a3ec21cc7eee9b17eaae
Sirinial <sirinial@gmail.com>
2021-12-12 21:11:43 +0100

    Template update

--- template/cpp/main.cpp
+++ template/cpp/main.cpp
@@ -1,31 +1,28 @@
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <map>
-#include <vector>
 #include <algorithm>
-#include <sstream>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
 
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    if (argc != 2)
+    if(argc != 2)
     {
         std::cerr << "Missing argument!" << std::endl;
         return 1;
     }
     const std::string fileName = argv[1];
 
-    std::ifstream infile(fileName);   
+    std::ifstream infile(fileName);
     std::cout << "Reading " << fileName << std::endl;
-    
+
     std::string line;
-    while (infile >> line)
+    while(infile >> line)
     {
         std::cout << line << std::endl;
     }
-    
-    
+
     return 0;
 }
